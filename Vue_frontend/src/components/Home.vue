428a9e1e0eec9db97d3862aa32ddb6ff4e1f36ab
ujjwalnahar <ujjwal.nahar121@gmail.com>
2022-05-28 00:25:54 +0530

    Vue Commit 1

---
+++ Vue_frontend/src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>
+            <h1>Django+Vue</h1>
+            <h2>
+                {{items.message}}
+            </h2>
+        </div>
+    </div>
+</template>
+<script>
+/* eslint-disable */
+    export default {
+        data() {
+            return {
+                
+                items: ['']
+            }
+        },
+        methods: {
+            async getData() {
+                try {
+                    
+                    const response = await this.$http.get('http://localhost:8080/greeting')
+                    
+                    this.items = response.data
+                    console.log(this.items.message)
+                } catch (error) {
+                    // log the error
+                    console.log(error);
+                }
+            }
+        },
+        created() {
+            
+            this.getData()
+        }
+    }
+</script>
